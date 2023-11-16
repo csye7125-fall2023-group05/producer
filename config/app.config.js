37b5b692fdfb051a084530c83431acc897ec5427
Siddharth Rawat <rawat.sid@northeastern.edu>
2023-11-15 22:03:42 -0500

    feat: add fetch function to get the health status check
    The `fetch()` will perform a GET request to get the status code for the
    specified URl. This data will then be pushed into the `consumer` db.

--- config/app.config.js
+++ config/app.config.js
@@ -1,12 +1,13 @@
 import 'dotenv/config'
 
-const { BROKER_0, BROKER_1, BROKER_2, CLIENT_ID, TOPIC } = process.env
+const { BROKER_0, BROKER_1, BROKER_2, CLIENT_ID, TOPIC, URL } = process.env
 const producerConfig = {
   BROKER_0,
   BROKER_1,
   BROKER_2,
   CLIENT_ID,
   TOPIC,
+  URL,
 }
 
 export default producerConfig
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Kafka } from 'kafkajs'
 import logger from './config/logger.config.js'
 import producerConfig from './config/app.config.js'
 
-const { BROKER_0, BROKER_1, BROKER_2, CLIENT_ID, TOPIC } = producerConfig
+const { BROKER_0, BROKER_1, BROKER_2, CLIENT_ID, TOPIC, URL } = producerConfig
 const topic = TOPIC
 const brokers = [BROKER_0, BROKER_1, BROKER_2]
 
@@ -16,15 +16,17 @@ const init = async () => {
     const producer = kafka.producer()
     await producer.connect()
     // TODO: get data dynamically for "healthcheck"
+    const response = await fetch(URL)
+    const status = await response.status
     const data = {
-      name: 'string',
-      uri: 'string',
+      name: 'google',
+      uri: URL,
       is_paused: true,
       num_retries: 5,
       uptime_sla: 100,
       response_time_sla: 100,
       use_ssl: true,
-      response_status_code: 0,
+      response_status_code: status,
       check_interval_in_seconds: 86400,
     }
     const result = await producer.send({
